49473edac7d08134a338581e70c9d6763609e523
zhenjianlee <zhenjianlee@outlook.sg>
2024-08-17 18:00:43 +0800

    updated entity relationships

--- 3_data_models/book_store/admin.py
+++ 3_data_models/book_store/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book,Author
 
 # Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=("first_name","last_name")
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields={"slug":("title",)}
+    list_filter=("author","rating")
+    list_display = ("title","author")
+
+
+admin.site.register(Author,AuthorAdmin)
+admin.site.register(Book, BookAdmin)
--- 3_data_models/book_store/models.py
+++ 3_data_models/book_store/models.py
@@ -6,23 +6,37 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Address(models.Model):
+    street = models.CharField(max_length=200)
+    postal_code = models.CharField(max_length=100)
+    city= models.CharField(max_length=100)
+    author = models.one_to_one_field(Author,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.street, self.postal_code, self.city}"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return f"first_name: {self.first_name} , last_name:{self.last_name}"
+
 class Book(models.Model):
     # id is automatically added by django
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(5)]
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
     )
-    author = models.CharField(null=True, max_length=100)
+    author=models.ForeignKey(Author,on_delete=models.CASCADE,null=True,related_name="books")
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False,db_index=True)
+    slug = models.SlugField(default="",blank=True, null=False,db_index=True)
 
     def get_absolute_url(self):
         #return reverse("book",args=[self.id]) #using id
         return reverse("book",args=[self.slug])
     
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args,**kwargs)
-
     def __str__(self):
-        return f"Book =>Title: {self.title}, Rating: {self.rating} Author: {self.author} Bestseller: {self.is_bestselling}"
+        return f"Title: {self.title},  Author: {self.author} Bestseller: {self.is_bestselling}"
