0ebf1f5cec5d4a1b77a6c1bea4ba922b3ed6e050
zhenjianlee <zhenjianlee@outlook.sg>
2024-08-18 17:20:28 +0800

    updated mysite with models , added forms

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Author,Post,Tag
 
 # Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    fields=('first_name','last_name','email')
+
+class PostAdmin(admin.ModelAdmin):
+    fields=('author','title','excerpt','image_name','date','slug','content')
+    list_selected_related=['author','tag']
+    list_filter=['author','title','date']
+
+class TagAdmin(admin.ModelAdmin):
+    fields=('posts','caption',)
+
+admin.site.register(Author,AuthorAdmin)
+admin.site.register(Post,PostAdmin)
+admin.site.register(Tag,TagAdmin)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return f"{self.first_name}, {self.last_name} , {self.email}"
+
+    class Meta:
+        pass
+
+class Post(models.Model):
+    author = models.ForeignKey(Author,on_delete=models.CASCADE,related_name="posts")
+    title= models.CharField(max_length=200)
+    excerpt=models.CharField(max_length=200)
+    image_name=models.CharField(max_length=200)
+    date=models.DateField()
+    slug=models.CharField(max_length=100)
+    content=models.TextField()
+
+    def __str__(self):
+        return f"{self.title} , {self.excerpt} , {self.date}"
+
+    class Meta:
+        pass
+
+
+class Tag(models.Model):
+    posts=models.ManyToManyField(Post,related_name="posts")
+    caption=models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.caption}"
+
+    class Meta:
+        pass
